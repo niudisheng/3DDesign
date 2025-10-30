90c350df5596d7580f2172e2816ddc92812a4a4d
niudisheng <1700849468@qq.com>
2025-10-30 15:54:45 +0800

    存

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,21 +1,29 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using State = PlayerStateController.State;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    [Header("Movement Settings")]
-    public float moveSpeed = 5f;
+    [Header("Movement Settings")] public float moveSpeed = 5f;
+    public float dashSpeed = 10f;
     public float jumpForce = 12f;
     public Transform groundCheck;
     public float groundCheckRadius = 0.2f;
     public LayerMask groundLayer;
 
+    public PlayerStateController playerStateController;
+
     private Rigidbody2D rb;
     private PlayerControls controls;
-    private Vector2 moveInput;
+    private Vector2 moveInputVector2;
     public bool isJumpPressed;
     public bool isGrounded;
+
+    private bool isDashing = false;
+    private int faceDir = 1; // 1 向右，-1 向左
+    private float dashDuration = 0.5f;
 
     void Awake()
     {
@@ -23,9 +31,10 @@ public class PlayerController : MonoBehaviour
         controls = new PlayerControls();
 
         // 监听输入
-        controls.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
-        controls.Player.Move.canceled += ctx => moveInput = Vector2.zero;
+        controls.Player.Move.performed += ctx => moveInputVector2 = ctx.ReadValue<Vector2>();
+        controls.Player.Move.canceled += ctx => moveInputVector2 = Vector2.zero;
         controls.Player.Jump.performed += ctx => isJumpPressed = true;
+        controls.Player.Dash.performed += ctx => TryDash();
     }
 
     void OnEnable() => controls.Player.Enable();
@@ -33,30 +42,16 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        // 检测是否在地面上
-        // isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
         isGrounded = true; // 跳过检测，直接假设在地面上
         // 跳跃输入检测
         if (isJumpPressed && isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
+
         isJumpPressed = false; // 重置跳跃输入
     }
 
-    void FixedUpdate()
-    {
-        // 移动（只影响x轴速度）
-        float moveX = moveInput.x * moveSpeed;
-        Debug.Log(moveX);
-        rb.velocity = new Vector2(moveX, rb.velocity.y);
-
-        // 可选：面向移动方向翻转角色
-        if (moveX > 0.1f)
-            transform.localScale = new Vector3(1, 1, 1);
-        else if (moveX < -0.1f)
-            transform.localScale = new Vector3(-1, 1, 1);
-    }
 
     void OnDrawGizmosSelected()
     {
@@ -65,5 +60,52 @@ public class PlayerController : MonoBehaviour
             Gizmos.color = Color.yellow;
             Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
         }
+    }
+
+    void FixedUpdate()
+    {
+        if (moveInputVector2.x == 0)
+        {
+            PlayerStateController.Instance.ChangeState(State.Idle);
+        }
+
+        if (!isDashing)
+            Move(); // 冲刺时禁止 Move()
+    }
+
+    private void Move()
+    {
+        playerStateController.ChangeState(State.Walk);
+        
+        float moveX = moveInputVector2.x * moveSpeed;
+        rb.velocity = new Vector2(moveX, rb.velocity.y);
+
+        // 翻转朝向（只有移动时才改变）
+        if (moveX > 0.1f)
+        {
+            faceDir = 1;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (moveX < -0.1f)
+        {
+            faceDir = -1;
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+    private void TryDash()
+    {
+        if (!isDashing)
+            StartCoroutine(DashCoroutine());
+    }
+
+    private IEnumerator DashCoroutine()
+    {
+        isDashing = true;
+        rb.velocity = new Vector2(faceDir * dashSpeed, 0f); // 冲刺锁定方向，并锁 y
+
+        yield return new WaitForSeconds(dashDuration);
+
+        isDashing = false;
     }
 }
--- Assets/Scripts/PlayerStateControler.cs
+++ Assets/Scripts/PlayerStateControler.cs
@@ -1,11 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
-using UnityEngine.Serialization;
 
-public class PlayerStateControler : MonoBehaviour
+public class PlayerStateController : MonoBehaviour
 {
-    enum State
+    public static PlayerStateController Instance;
+
+    public enum State
     {
         Idle,
         Walk,
@@ -16,140 +16,45 @@ public class PlayerStateControler : MonoBehaviour
     }
 
     private State currentState;
-    private Rigidbody2D rb;
+    private Animator animator;
 
-    // 玩家状态的相关参数
-    public float walkSpeed = 2f;
-    public float dashSpeed = 5f;
-    public float jumpSpeed = 10f;
-    public float attackTime = 1f;
-    private float attackCooldown = 0;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
-        currentState = State.Idle;
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        animator = GetComponent<Animator>();
+        ChangeState(State.Idle);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeState(State newState)
     {
-        // 如果攻击冷却时间大于0，则减少冷却时间
-        if (attackCooldown > 0)
-        {
-            attackCooldown -= Time.deltaTime;
-        }
+        if (currentState == newState)
+            return;
 
-        // 根据输入改变状态
-        HandleState();
+        currentState = newState;
+        PlayAnimationByState(newState);
     }
 
-    void HandleState()
+    private void Update()
     {
-        switch (currentState)
-        {
-            case State.Idle:
-                Idle();
-                break;
-            case State.Walk:
-                Walk();
-                break;
-            case State.Dash:
-                Dash();
-                break;
-            case State.Attack:
-                Attack();
-                break;
-            case State.Down:
-                Down();
-                break;
-            case State.Jump:
-                Jump();
-                break;
-        }
+        PlayAnimationByState(currentState);
     }
 
-    // 闲置状态
-    void Idle()
+    private void PlayAnimationByState(State state)
     {
-        // 如果按下了移动键，就进入行走状态
-        if (Input.GetAxisRaw("Horizontal") != 0)
-        {
-            currentState = State.Walk;
-        }
-
-        // 如果按下跳跃键，就进入跳跃状态
-        if (Input.GetButtonDown("Jump"))
-        {
-            currentState = State.Jump;
-        }
+        Debug.Log("Player State: " + state);
+        animator.SetBool(state.ToString(),true);
     }
 
-    // 行走状态
-    void Walk()
+    public State GetCurrentState()
     {
-        float moveInput = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(moveInput * walkSpeed, rb.velocity.y);
-
-        // 如果松开方向键，就回到闲置状态
-        if (moveInput == 0)
-        {
-            currentState = State.Idle;
-        }
-
-        // 如果按下跳跃键，就进入跳跃状态
-        if (Input.GetButtonDown("Jump"))
-        {
-            currentState = State.Jump;
-        }
-
-        // 如果按下冲刺键，就进入冲刺状态
-        if (Input.GetButtonDown("Dash"))
-        {
-            currentState = State.Dash;
-        }
-    }
-
-    // 冲刺状态
-    void Dash()
-    {
-        float moveInput = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(moveInput * dashSpeed, rb.velocity.y);
-
-        // 冲刺结束后回到闲置状态
-        if (Input.GetButtonDown("Dash") || moveInput == 0)
-        {
-            currentState = State.Idle;
-        }
-    }
-
-    // 攻击状态
-    void Attack()
-    {
-        if (attackCooldown <= 0)
-        {
-            // 执行攻击行为
-            Debug.Log("Attacking...");
-            // 设置攻击冷却
-            attackCooldown = attackTime;
-        }
-
-        // 完成攻击后回到闲置状态
-        currentState = State.Idle;
-    }
-
-    // 跌倒状态
-    void Down()
-    {
-        // 这里可以添加跌倒时的行为
-        // 比如设置速度为0或播放跌倒动画
-    }
-
-    // 跳跃状态
-    void Jump()
-    {
-        rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
-        currentState = State.Idle;
+        return currentState;
     }
 }
